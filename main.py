a3c1217cf8e5e5ea9c442126e92f286e649e4331
Admin <btr919@gmail.com>
2025-08-15 18:07:26 +0300

    added flowchart video and modify the code and used the strategy pattern

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
     if platform.system() != "Windows":
         print("This system monitor currently supports only Windows OS.")
         sys.exit(1)
-    q = Queue()
-    z = Queue()
-    sysinfo = SystemInfoService(q, z)
-    sysinfo_presentor = SystemInfoPresentorService(queue=z)
+    monitor_ui_queue = Queue()
+    system_presentor_queue = Queue()
+    sysinfo = SystemInfoService(monitor_ui_queue, system_presentor_queue)
+    sysinfo_presentor = SystemInfoPresentorService(queue=system_presentor_queue)
     sysinfo.start()
     sysinfo_presentor.start()
 
 
     app = QtWidgets.QApplication(sys.argv)
-    ui = MonitorUI(q)
+    ui = MonitorUI(monitor_ui_queue)
     try:
         ui.run()
         exit_code = app.exec_()
